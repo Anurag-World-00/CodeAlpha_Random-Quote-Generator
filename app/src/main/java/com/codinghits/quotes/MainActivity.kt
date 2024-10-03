69a8e1e9b9492ae9e5f9112fa454d5c205fe66ad
Anurag <asbhaman0@gmail.com>
2024-10-03 17:48:10 +0530

    App has completed

--- app/src/main/java/com/codinghits/quotes/MainActivity.kt
+++ app/src/main/java/com/codinghits/quotes/MainActivity.kt
@@ -11,48 +11,52 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import org.w3c.dom.Text
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textView : TextView
-    lateinit var button : Button
-    lateinit var share : Button
+    lateinit var textView: TextView
+    lateinit var button: Button
+    lateinit var share: Button
+    lateinit var progressBar: ProgressBar  // ProgressBar declaration
     val collectDataInSB = StringBuilder()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Initialize views
         textView = findViewById(R.id.textView)
         button = findViewById(R.id.button)
         share = findViewById(R.id.Share)
+        progressBar = findViewById(R.id.progressBar)  // Initialize the ProgressBar
 
-        button.setOnClickListener{
+        // Set up button click listeners
+        button.setOnClickListener {
             retroData()
         }
-        share.setOnClickListener{
+
+        share.setOnClickListener {
             val intent = Intent(Intent.ACTION_SEND)
             intent.type = "text/plain"
-            intent.putExtra(Intent.EXTRA_TEXT,"$collectDataInSB")
-            val chooser = Intent.createChooser(intent,"Share this quote using...")
+            intent.putExtra(Intent.EXTRA_TEXT, "$collectDataInSB")
+            val chooser = Intent.createChooser(intent, "Share this quote using...")
             startActivity(chooser)
         }
-
-
     }
-    private fun retroData(){
 
+    private fun retroData() {
+        progressBar.visibility = View.VISIBLE  // Show the ProgressBar
 
         val retrofitBuilder = Retrofit.Builder()
             .baseUrl("https://zenquotes.io/api/")
@@ -66,19 +70,23 @@ class MainActivity : AppCompatActivity() {
                 call: Call<List<QuoteData>?>,
                 response: Response<List<QuoteData>?>
             ) {
-                val productList = response.body()!!
+                progressBar.visibility = View.GONE  // Hide the ProgressBar on response
+                if (response.body() != null) {
+                    val productList = response.body()!!
 
-                for(myData in productList){
-                    collectDataInSB.replace(0,collectDataInSB.length,myData.q + " ")
+                    for (myData in productList) {
+                        collectDataInSB.replace(0, collectDataInSB.length, myData.q + " ")
+                    }
+                    textView.text = collectDataInSB
+                } else {
+                    Toast.makeText(this@MainActivity, "No data found", Toast.LENGTH_LONG).show()
                 }
-                textView.text = collectDataInSB
-
             }
 
             override fun onFailure(call: Call<List<QuoteData>?>, t: Throwable) {
-                Toast.makeText(this@MainActivity,"Failure${t.message}",Toast.LENGTH_LONG).show()
+                progressBar.visibility = View.GONE  // Hide the ProgressBar on failure
+                Toast.makeText(this@MainActivity, "Failure: ${t.message}", Toast.LENGTH_LONG).show()
             }
         })
-
     }
 }
